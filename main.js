bfec714562679881254be128362e8ca04d2b405b
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-07-24 17:05:20 +0200

    add cross for deletion to note component

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import "./style.css";
-import { addNote } from "./src/components/note";
+import { addNote, removeNote } from "./src/components/note";
 
-window.addEventListener("click", addNote);
+window.addEventListener("click", (event) => {
+  if (event.target.classList.contains("note-deleter")) {
+    removeNote(event.target.parentElement);
+  } else {
+    addNote();
+  }
+});
--- src/components/note.js
+++ src/components/note.js
@@ -1,9 +1,21 @@
 import "./note.css";
 
-const note = `
-<textarea class="note" placeholder="text here" value="text here"></textarea>
-`;
+function createNote() {
+  const noteContents = `
+    <div class="note-deleter"></div>
+    <textarea class="note-text" placeholder="text here" value="text here"></textarea>
+  `;
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = noteContents;
+
+  return note;
+}
 
 export function addNote() {
-  document.querySelector("#app").insertAdjacentHTML("beforeend", note);
+  document.querySelector("#app").appendChild(createNote());
+}
+
+export function removeNote(note) {
+  document.querySelector("#app").removeChild(note);
 }
