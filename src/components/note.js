68c8d7b49bcf9954a742a39253453a14a8062a1e
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-07-25 13:27:41 +0200

    change layout to absolutely positioned and spawn note on mouse cursor

--- src/components/note.js
+++ src/components/note.js
@@ -1,20 +1,27 @@
 import "./note.css";
 
-function createNote() {
+const headerHeight = document
+  .querySelector("header")
+  .getBoundingClientRect().height;
+
+function createNote(x, y) {
   const noteContents = `
     <div class="note-deleter"></div>
     <textarea class="note-text" placeholder="text here" value="text here"></textarea>
   `;
+  y = y - headerHeight;
   const note = document.createElement("div");
   note.classList.add("note");
   note.innerHTML = noteContents;
-  note.setAttribute('draggable', 'true');
+  note.style.left = `${x}px`;
+  note.style.top = `${y}px`;
+  note.setAttribute("draggable", "true");
 
   return note;
 }
 
-export function addNote() {
-  document.querySelector(".note-gallery").appendChild(createNote());
+export function addNote(x, y) {
+  document.querySelector(".note-gallery").appendChild(createNote(x, y));
 }
 
 export function removeNote(note) {
