a62f6a88418bdf5833a1820223d21bdc270123f4
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-07-25 21:50:16 -0400

    added double click to make note appear

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 import "./style.css";
 import { addNote, removeNote } from "./src/components/note";
 
-window.addEventListener("click", (event) => {
+window.addEventListener("dblclick", (event) => {
   if (event.target.classList.contains("note-text")) return;
+  
+  addNote(event.pageX, event.pageY);
+});
+
+window.addEventListener("click", (event) => {
   if (event.target.classList.contains("note-deleter")) {
     removeNote(event.target.parentElement);
     return;
   }
-  addNote(event.pageX, event.pageY);
 });
+
+
+
+
 
 //class for notes = "note"
