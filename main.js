7a71de48eeca70d0085796e0c8be4e6ac6aa4ad3
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-07-25 22:06:49 -0400

    added ability to create note and have it appear near middle of page

--- main.js
+++ main.js
@@ -14,6 +14,12 @@ window.addEventListener("click", (event) => {
   }
 });
 
+document.querySelector('#add-button').addEventListener("click", (event) => {
+  if (event.target.classList.contains("note-text")) return;
+  
+  addNote(event.pageX/2.25, event.pageY/2.25);
+});
+
 
 
 
